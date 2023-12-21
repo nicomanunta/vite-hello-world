f98018e8a97890ad23d18be9609f5edf58e52d33
nicomanunta <nic.manun@gmail.com>
2023-12-21 16:32:06 +0100

    esercizio completato (credo)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script>
+import AppTitle from "./components/AppTitle.vue";
 export default {
-
+    components: {
+        AppTitle
+    }
 }
 </script>
 <template lang="">
     <div>
-        
+        <AppTitle/>
     </div>
 </template>
 <style lang="">
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -5,7 +5,7 @@ export default {
 </script>
 <template lang="">
     <div>
-        
+        <h1>La mia prima app con Vite!</h1>
     </div>
 </template>
 <style lang="">
